94905ced0cd7ea9f2f86682ae32048ea1020616a
Bruno <brunocfaraco@hotmail.com.br>
2022-05-18 17:42:06 -0300

    conclusão do requisito obrigatório de nº6

--- src/challenges.js
+++ src/challenges.js
@@ -31,9 +31,22 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(array) {
+  let higherNumber = array[0];
+  let higherNumberCounter = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (higherNumber < array[index]) {
+      higherNumber = array[index];
+    };
+  }
+  for (let index2 = 0; index2 < array.length; index2 += 1) {
+    if (higherNumber === array[index2]) {
+      higherNumberCounter += 1;
+    };
+  };
+  return higherNumberCounter;
+} 
 
 // Desafio 7
 function catAndMouse() {
